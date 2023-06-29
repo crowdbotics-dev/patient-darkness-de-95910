917f88e9471d98cb42fc1b0d78a0c4151f5754f9
Crowdbotics <engineering@crowdbotics.com>
2023-06-29 10:32:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "patient-darkness-de-95910",
+  url: "https://patient-darkness-de-95910.botics.co",
+  api: "https://patient-darkness-de-95910.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
